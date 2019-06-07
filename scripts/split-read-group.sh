7ccaf3a7176ac52c0d68162b4af0f5affc4d3b45
kdaily <kenneth.daily@sagebase.org>
2019-06-07 14:00:56 -0700

    make it work and document it.

--- scripts/split-read-group.sh
+++ scripts/split-read-group.sh
@@ -1,43 +1,51 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Split a fastq file based on read header information.
-# Outputs as a gzipped fastq file.
+#
+# Assumes the format of the header is:
+# @<machine>:<run>:<flowcell>:<lane>:<tile><x_coord>:<y_coord> <read_mate_number>:<vendor_filtered>:<bits>:<barcode>
+#
+# Outputs as a gzipped fastq file. Output prefix will be used for the output files 
+# created which will be of the form <prefix><flowcell>_<lane>_R<1/2>.fastq.gz
 
-if [[ $# -lt 3 ]]; then
-    echo "Usage: $(basename $0) fastq prefix outputdir"
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $(basename $0) fastq prefix"
     false
 fi
 
-FQ=$1
-SM=$2
-OUTDIR=$3
+FASTQ=$1
+OUTPREFIX=$2
 
-if [[ $FQ == *.gz ]]; then
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${FASTQ}" = *.gz ]]; then
     CAT=zcat
-elif [[ $FQ == *.fastq ]]; then
+elif [[ "${FASTQ}" = *.fastq ]]; then
     CAT=cat
 else
-    exit 1
+    printf "Not a fastq or gzip file, exiting."
+    false
 fi
 
-if [[ $FQ =~ (.R1|_R1|_r1|_1)(|_001).f(|ast)q(|.gz) ]]; then
-    RD=R1
-elif [[ $FQ =~ (.R1|_R1|_r1|_1)(|_001).f(|ast)q(|.gz) ]]; then
-    RD=R2
-else
-    exit 1
-fi
+printf -- "[$(date)] Start split fastq: ${FASTQ}\n"
 
-printf -- "---\n[$(date)] Start split fastq: $FQ\n"
+${CAT} ${FASTQ} | paste - - - - | awk -F"\t" -v OUTPREFIX=${OUTPREFIX} '{
+    header=$1;
+    sub(/^@/,"",header);
 
-$CAT $FQ |paste - - - - |awk -F"\t" -v SM=$SM -v RD=$RD '{
-    h=$1;
-    sub(/^@/,"",h);
-    sub(/ .+$/,"",h);
-    l=split(h,arr,":");
-    FCX=arr[l-4];
-    LN=arr[l-3];
-    print $1"\n"$2"\n+\n"$4|"gzip > "OUTDIR"/"SM"."FCX"_L"LN"."RD".fastq.gz"}
-    END {
-    print "READ N: "NR}'
+    x = split(header, arr1, " ");
+    part1 = arr1[1];
+    part2 = arr1[2];
 
-printf -- "[$(date)] Finish split fastq: $FQ\n---\n"
+    l = split(part1, arr2, ":");
+    FLOWCELL = arr2[3];
+    LANE = arr2[4];
+    
+    m = split(part2, arr3, ":")
+    READ = arr3[1];
+
+    print $1 "\n" $2 "\n" $3 "\n" $4 > OUTPREFIX "_" FLOWCELL "_" LANE "_R" READ ".fastq.gz"}'
+
+
+printf -- "[$(date)] Finish split fastq: ${FASTQ}\n---\n"
